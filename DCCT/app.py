62e19a693832a6be933fb10aa2e1ae299f890824
allefpablo <allefpablo@gmail.com>
2017-07-03 17:35:10 -0300

    Add dmreader and update app.py

--- DCCT/app.py
+++ DCCT/app.py
@@ -4,23 +4,27 @@ from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QWizard, QApplication
 
+
 class DCCTWindow(QWizard):
 
     def __init__(self, parent=None):
         super(DCCTWindow, self).__init__(parent)
         uic.loadUi('wizard.ui', self)
 
+        self.finished.connect(self._finish_wizard_execution)
+
         self._SERIAL_BAUDRATE = 115200
 
         self._initialize_widgets()
         self._initialize_signals()
-
         self._initialize_wizard_buttons()
+        self._block_buttons()
+
 
     @pyqtSlot()
     def _read_serial_number(self):
         #TODO: Read serial number routine
-        print("Read Serial Number")
+        print("Read serial number")
 
     @pyqtSlot()
     def _treat_read_serial_edit(self):
@@ -31,18 +35,23 @@ class DCCTWindow(QWizard):
 
     @pyqtSlot()
     def _connect_serial_port(self):
+        self.PageConnectSerialPort.wizard().button(self.NextButton).setEnabled(True)
         #TODO: Connect and test serial port
-        pass
 
     @pyqtSlot()
     def _start_test_sequence(self):
+        self.PageStartTest.wizard().button(self.NextButton).setEnabled(True)
         #TODO: Start test sequence thread
-        pass
 
     @pyqtSlot()
     def _submit_test_report(self):
+        self.PageSubmitReport.wizard().button(self.FinishButton).setEnabled(True)
         #TODO: Submit report to server
-        pass
+
+    @pyqtSlot()
+    def _finish_wizard_execution(self):
+        # Qdo usuario clica em cancel ou em finish
+        print("*****TERMINOU******")
 
     def _initialize_widgets(self):
         """ Initial widgets configuration """
@@ -57,6 +66,7 @@ class DCCTWindow(QWizard):
         self.pbConnectSerialPort.clicked.connect(self._connect_serial_port)
         self.pbStartTests.clicked.connect(self._start_test_sequence)
         self.pbSubmitTestReport.clicked.connect(self._submit_test_report)
+        self.finished.connect(self._finish_wizard_execution)
 
     def _initialize_wizard_buttons(self):
         self.PageIntro.setButtonText(self.NextButton, "Próximo")
@@ -82,6 +92,9 @@ class DCCTWindow(QWizard):
         self.PageSubmitReport.setButtonText(self.BackButton, "Anterior")
         self.PageSubmitReport.setButtonText(self.CancelButton, "Cancelar")
         self.PageSubmitReport.setButtonText(self.FinishButton, "Novo Teste")
+
+    def _block_buttons(self):
+        self.PageSubmitReport.wizard().button(self.FinishButton).setEnabled(False)
 
 #app = QApplication(sys.argv)
 #GUI = Window()
---
+++ DCCT/dmreader/dmreader.py
@@ -0,0 +1,81 @@
+'''
+pip3 install pylibdmtx
+pip3 install pylibdmtx[scripts]
+pip3 install opencv-python
+'''
+
+import cv2
+from pylibdmtx.pylibdmtx import decode
+
+def ReadDataMatrix(frameWidth = 640, frameHeight = 480, cropSize = 220):
+
+    print('*************************************************************************')
+    print('Posicione o barcode dentro do quadrado e selecione uma das opções abaixo:')
+    print('*************************************************************************')
+    print('\nSPACE: Ler etiqueta')
+    print('ESC: Cancelar')
+
+    # Scan area parameters
+    y0 = int((frameHeight-cropSize)/2)
+    yEnd = int((frameHeight+cropSize)/2)
+    x0 = int((frameWidth-cropSize)/2)
+    xEnd = int((frameWidth+cropSize)/2)
+    horizLine = [0 for val in range(x0,xEnd)]
+    vertLine = [0 for val in range(y0,yEnd)]
+
+    # Webcam initialization
+    cam = cv2.VideoCapture(0)
+    cam.set(3,frameWidth)
+    cam.set(4,frameHeight)
+
+    while True:
+        # Read and process frame
+        ret_val, img = cam.read()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.flip(img, 0)
+        img = cv2.flip(img, 1)
+
+        # Crop frame to scan area
+        crop_img = img[y0:yEnd,x0:xEnd]
+
+        # Draw scan area
+        img[y0:yEnd,x0] = vertLine
+        img[y0:yEnd,xEnd] = vertLine
+        img[y0,x0:xEnd] = horizLine
+        img[yEnd,x0:xEnd] = horizLine
+
+        # Show video
+        cv2.imshow('Place barcode inside the square', img)
+
+        # Esc to quit
+        # Space to read barcode
+        pressedKey = cv2.waitKey(1)
+        if pressedKey != 255:
+            print(pressedKey)
+        #if pressedKey == 81 or pressedKey == 113:
+        if pressedKey == 27:
+            data = None
+            break
+        #elif pressedKey == 67 or pressedKey == 99:
+        elif pressedKey == 32:
+            data = decode(crop_img)
+            if data == []:
+                print('Tente novamente...')
+            else:
+                data = int(decode(crop_img)[0].data)
+                break
+
+    # Release camera application
+    cam.release()
+    cv2.destroyAllWindows()
+    return data
+
+#def main():
+#    data = ReadDataMatrix()
+#    if data == None:
+#        print('Leitura cancelada')
+#    else:
+#        print('Serial-Number lido: ' + str(data))
+#    
+#if __name__ == '__main__':
+#    main()
