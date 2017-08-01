7b2f8e08d38d552379d023ac8db58085b8a76c60
allefpablo <allefpablo@gmail.com>
2017-08-01 17:29:42 -0300

    Update. New parameter for LogUdc

--- udc/app.py
+++ udc/app.py
@@ -13,9 +13,12 @@ Ui_Class, base = loadUiType(UI_PATH)
 
 class UDCWindow(QWizard, Ui_Class):
 
+    FAIL        = "Falha"
+    SUCCESS     = "OK"
+
     # Page numbers
     (num_intro_page, num_serial_number, num_connect_udc,
-    num_serial_port, num_start_test)     = range(5)
+    num_visual_test, num_serial_port, num_start_test) = range(6)
 
     def __init__(self, parent=None):
         QWizard.__init__(self, parent)
@@ -45,8 +48,33 @@ class UDCWindow(QWizard, Ui_Class):
         self.lbReadSerialStatus.clear()
         self.cbEnableSerialNumberEdit.setChecked(False)
         self.lbStatusComunicacao.setText("...")
-        self.lbTestStatus.setText("Clique para Iniciar Testes")
+        self.rbLedsOk.setChecked(False)
+        self.rbLedsNok.setChecked(False)
+        self.rbBuzzerOk.setChecked(False)
+        self.rbBuzzerNok.setChecked(False)
+        self.lbTestStatus.setText("Iniciar...")
         self.lbTestResult.setText("Aguarde...")
+        self.lbEeprom.setText("...")
+        self.lbFlash.setText("...")
+        self.lbRam.setText("...")
+        self.lbAdc1.setText("...")
+        self.lbAdc2.setText("...")
+        self.lbAdc3.setText("...")
+        self.lbAdc4.setText("...")
+        self.lbAdc5.setText("...")
+        self.lbAdc6.setText("...")
+        self.lbAdc7.setText("...")
+        self.lbAdc8.setText("...")
+        self.lbRtcCom.setText("...")
+        self.lbRtcInt.setText("...")
+        self.lbSensorTempCom.setText("...")
+        self.lbSensorTempTemp.setText("...")
+        self.lbRs4851.setText("...")
+        self.lbRs4852.setText("...")
+        self.lbRs4853.setText("...")
+        self.lbAlimPlanoIsolado.setText("...")
+        self.lbExpansorIo.setText("...")
+        self.lbEthernet.setText("...")
         self.teTestReport.clear()
 
     def _initialize_signals(self):
@@ -71,6 +99,10 @@ class UDCWindow(QWizard, Ui_Class):
         self.PageConnectUDC.setButtonText(self.NextButton, "Próximo")
         self.PageConnectUDC.setButtonText(self.BackButton, "Anterior")
         self.PageConnectUDC.setButtonText(self.CancelButton, "Cancelar")
+
+        self.PageVisualTest.setButtonText(self.NextButton, "Próximo")
+        self.PageVisualTest.setButtonText(self.BackButton, "Anterior")
+        self.PageVisualTest.setButtonText(self.CancelButton, "Cancelar")
 
         self.PageTestSerialPort.setButtonText(self.NextButton, "Próximo")
         self.PageTestSerialPort.setButtonText(self.BackButton, "Anterior")
@@ -102,9 +134,40 @@ class UDCWindow(QWizard, Ui_Class):
             except (OSError, serial.SerialException):
                 pass
 
-    def _restart_test_thread(self):
+    def _connect_test_signals(self):
+        self._test_thread.test_complete.connect(self._test_finished)
+        self._test_thread.update_gui.connect(self._update_test_log)
+        self._test_thread.eeprom.connect(self._update_eeprom_label)
+        self._test_thread.flash.connect(self._update_flash_label)
+        self._test_thread.ram.connect(self._update_ram_label)
+        self._test_thread.adc.connect(self._update_adc_label)
+        self._test_thread.rtc_com.connect(self._update_rtc_com_label)
+        self._test_thread.rtc_int.connect(self._update_rtc_int_label)
+        self._test_thread.sensor_temp_com.connect(self._update_sensor_temp_com_label)
+        self._test_thread.sensor_temp_val.connect(self._update_sensor_temp_val_label)
+        self._test_thread.rs485.connect(self._update_rs485_label)
+        self._test_thread.isol_plane.connect(self._update_isol_plane_label)
+        self._test_thread.io_expander.connect(self._update_io_expander_label)
+        self._test_thread.ethernet.connect(self._update_ethernet_label)
+
+    def _disconnect_test_signals(self):
         self._test_thread.test_complete.disconnect()
         self._test_thread.update_gui.disconnect()
+        self._test_thread.eeprom.disconnect()
+        self._test_thread.flash.disconnect()
+        self._test_thread.ram.disconnect()
+        self._test_thread.adc.disconnect()
+        self._test_thread.rtc_com.disconnect()
+        self._test_thread.rtc_int.disconnect()
+        self._test_thread.sensor_temp_com.disconnect()
+        self._test_thread.sensor_temp_val.disconnect()
+        self._test_thread.rs485.disconnect()
+        self._test_thread.isol_plane.disconnect()
+        self._test_thread.io_expander.disconnect()
+        self._test_thread.ethernet.disconnect()
+
+    def _finish_test_thread(self):
+        self._disconnect_test_signals()
         self._test_thread.quit()
         self._test_thread.wait()
 
@@ -120,6 +183,9 @@ class UDCWindow(QWizard, Ui_Class):
     def _initialize_page_connect_udc(self):
         pass
 
+    def _initialize_page_visual_test(self):
+        pass
+
     def _initialize_page_test_serial_port(self):
         pass
 
@@ -130,9 +196,8 @@ class UDCWindow(QWizard, Ui_Class):
     ************** Pages Validation ********************
     *************************************************"""
     def _validate_intro_page(self):
-        if self._serial_port_status:
-            return True
-        return False
+        #return self._serial_port_status
+        return True
 
     def _validate_page_serial_number(self):
         serial = self.leSerialNumber.text()
@@ -146,13 +211,31 @@ class UDCWindow(QWizard, Ui_Class):
     def _validate_page_connect_udc(self):
         return True
 
+    def _validate_page_visual_test(self):
+        if (self.rbLedsOk.isChecked() or self.rbLedsNok.isChecked()) and \
+            (self.rbBuzzerOk.isChecked() or self.rbBuzzerNok.isChecked()):
+
+            if self.rbLedsOk.isChecked():
+                self._test_thread.led = self.SUCCESS
+            else:
+                self._test_thread.led = self.FAIL
+
+            if self.rbBuzzerOk.isChecked():
+                self._test_thread.buzzer = self.SUCCESS
+            else:
+                self._test_thread.buzzer = self.FAIL
+
+            return True
+
+        return False
+
     def _validate_page_test_serial_port(self):
         return self._test_serial_port_status
 
     def _validate_page_start_test(self):
         print('Validate Start Test')
         self._initialize_widgets()
-        self._restart_test_thread()
+        self._finish_test_thread()
         while self.currentId() is not self.num_serial_number:
             self.back()
         return False
@@ -174,6 +257,9 @@ class UDCWindow(QWizard, Ui_Class):
             self._initialize_page_connect_udc()
             print(self.currentId())
 
+        elif page == self.num_visual_test:
+            self._initialize_page_visual_test()
+
         elif page == self.num_serial_port:
             self._initialize_page_test_serial_port()
             print(self.currentId())
@@ -191,19 +277,18 @@ class UDCWindow(QWizard, Ui_Class):
             return self._validate_intro_page()
 
         elif current_id == self.num_serial_number:
-            print("Valida 1")
             return self._validate_page_serial_number()
 
         elif current_id == self.num_connect_udc:
-            print("Valida 2")
             return self._validate_page_connect_udc()
 
+        elif current_id == self.num_visual_test:
+            return self._validate_page_visual_test()
+
         elif current_id == self.num_serial_port:
-            print("Valida 3")
             return self._validate_page_test_serial_port()
 
         elif current_id == self.num_start_test:
-            print("Valida 4")
             return self._validate_page_start_test()
 
         else:
@@ -258,8 +343,7 @@ class UDCWindow(QWizard, Ui_Class):
 
     @pyqtSlot()
     def _start_test_sequence(self):
-        self._test_thread.test_complete.connect(self._test_finished)
-        self._test_thread.update_gui.connect(self._update_test_log)
+        self._connect_test_signals()
         self._test_thread.start()
 
     @pyqtSlot()
@@ -276,6 +360,63 @@ class UDCWindow(QWizard, Ui_Class):
     @pyqtSlot(str)
     def _update_test_log(self, value):
         self.teTestReport.append(value)
+
+    @pyqtSlot(str)
+    def _update_eeprom_label(self, value):
+        self.lbEeprom.setText(value)
+
+    @pyqtSlot(str)
+    def _update_flash_label(self, value):
+        self.lbFlash.setText(value)
+
+    @pyqtSlot(str)
+    def _update_ram_label(self, value):
+        self.lbRam.setText(value)
+
+    @pyqtSlot(list)
+    def _update_adc_label(self, value):
+        self.lbAdc1.setText(value[0])
+        self.lbAdc2.setText(value[1])
+        self.lbAdc3.setText(value[2])
+        self.lbAdc4.setText(value[3])
+        self.lbAdc5.setText(value[4])
+        self.lbAdc6.setText(value[5])
+        self.lbAdc7.setText(value[6])
+        self.lbAdc8.setText(value[7])
+
+    @pyqtSlot(str)
+    def _update_rtc_com_label(self, value):
+        self.lbRtcCom.setText(value)
+
+    @pyqtSlot(str)
+    def _update_rtc_int_label(self, value):
+        self.lbRtcInt.setText(value)
+
+    @pyqtSlot(str)
+    def _update_sensor_temp_com_label(self, value):
+        self.lbSensorTempCom.setText(value)
+
+    @pyqtSlot(str)
+    def _update_sensor_temp_val_label(self, value):
+        self.lbSensorTempTemp.setText(value)
+
+    @pyqtSlot(list)
+    def _update_rs485_label(self, value):
+        self.lbRs4851.setText(value[0])
+        self.lbRs4852.setText(value[1])
+        self.lbRs4853.setText(value[2])
+
+    @pyqtSlot(str)
+    def _update_isol_plane_label(self, value):
+        self.lbAlimPlanoIsolado.setText(value)
+
+    @pyqtSlot(str)
+    def _update_io_expander_label(self, value):
+        self.lbExpansorIo.setText(value)
+
+    @pyqtSlot(str)
+    def _update_ethernet_label(self, value):
+        self.lbEthernet.setText(value)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
