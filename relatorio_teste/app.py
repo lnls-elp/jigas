d04b00cf57c2c024a70811b5afc79170af25ef90
allefpablo <allefpablo@gmail.com>
2017-08-25 13:36:30 -0300

    Add relatorio_test

--- relatorio_teste/app.py
+++ relatorio_teste/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QMessageBox
 from PyQt5.QtCore import pyqtSlot, pyqtSignal
 from PyQt5.uic import loadUiType
+from converter import Converter
 import sys
 
 UI_PATH = 'gui.ui'
@@ -12,8 +13,8 @@ class Report(QWidget, Ui_Class):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.setupUi(self)
-
         self._initialize_signals()
+        self._conv = Converter()
 
     """*************************************************
     *************** GUI Initialization *****************
@@ -27,42 +28,110 @@ class Report(QWidget, Ui_Class):
         self.pbPowerModule.clicked.connect(self._get_pm_report)
         self.pbUdc.clicked.connect(self._get_udc_report)
         self.pbPowerSupply.clicked.connect(self._get_ps_report)
-        self.pbBurnIn.clicked.connect(self._get_burn_in_report)
+        self.pbBurnIn.clicked.connect(self._get_ps_report)
 
     @pyqtSlot()
     def _get_hradc_report(self):
-        pass
+        url = '/HradcReport'
+        self._conv.resource = url
+        self._conv.conversion_complete.connect(self._clear)
+        self.pbHradc.setEnabled(False)
+        self.pbHradc.setText("Aguarde!")
+        self._conv.start()
 
     @pyqtSlot()
     def _get_hradc_calib_report(self):
-        pass
+        url = '/HradcCalibReport'
+        self._conv.resource = url
+        self._conv.conversion_complete.connect(self._clear)
+        self.pbCalibHradc.setText("Aguarde!")
+        self.pbCalibHradc.setEnabled(False)
+        self._conv.start()
 
     @pyqtSlot()
     def _get_dcct_report(self):
-        pass
+        url = '/DcctReport'
+        self._conv.resource = url
+        self._conv.conversion_complete.connect(self._clear)
+        self.pbDcct.setText("Aguarde!")
+        self.pbDcct.setEnabled(False)
+        self._conv.start()
 
     @pyqtSlot()
     def _get_rack_report(self):
-        pass
+        url = '/RackReport'
+        self._conv.resource = url
+        self._conv.conversion_complete.connect(self._clear)
+        self.pbRack.setText("Aguarde!")
+        self.pbRack.setEnabled(False)
+        self._conv.start()
 
     @pyqtSlot()
     def _get_pm_report(self):
-        pass
+        url = '/PowerModuleReport'
+        self._conv.resource = url
+        self._conv.conversion_complete.connect(self._clear)
+        self.pbPowerModule.setText("Aguarde!")
+        self.pbPowerModule.setEnabled(False)
+        self._conv.start()
 
     @pyqtSlot()
     def _get_udc_report(self):
-        pass
+        url = '/UdcReport'
+        self._conv.resource = url
+        self._conv.conversion_complete.connect(self._clear)
+        self.pbUdc.setText("Aguarde!")
+        self.pbUdc.setEnabled(False)
+        self._conv.start()
 
     @pyqtSlot()
     def _get_ps_report(self):
-        pass
+        url = '/PowerSupplyReport'
+        self._conv.resource = url
+        self._conv.conversion_complete.connect(self._clear)
+        self.pbPowerSupply.setText("Aguarde!")
+        self.pbPowerSupply.setEnabled(False)
+        self._conv.start()
 
-    @pyqtSlot()
-    def _get_burn_in_report(self):
-        pass
+    @pyqtSlot(bool)
+    def _clear(self, status):
+        self._conv.conversion_complete.disconnect()
+        self.pbHradc.setEnabled(True)
+        self.pbCalibHradc.setEnabled(True)
+        self.pbDcct.setEnabled(True)
+        self.pbRack.setEnabled(True)
+        self.pbPowerModule.setEnabled(True)
+        self.pbPowerSupply.setEnabled(True)
+        self.pbUdc.setEnabled(True)
+        self.pbBurnIn.setEnabled(True)
+        self.pbHradc.setText("HRADC")
+        self.pbCalibHradc.setText("HRADC\nCalibração")
+        self.pbDcct.setText("DCCT")
+        self.pbRack.setText("Bastidor")
+        self.pbPowerModule.setText("Módulo\nPotência")
+        self.pbPowerSupply.setText("Fonte\nCompleta")
+        self.pbUdc.setText("UDC")
+        self.pbBurnIn.setText("Burn-In")
+
+        if status:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("Sucesso")
+            msg.setInformativeText("Sucesso: Veja a pasta relatorios")
+            msg.setWindowTitle("Sucesso")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("ERRO")
+            msg.setInformativeText("ERRO: Dados Não existem")
+            msg.setWindowTitle("ERRO")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     gui = Report()
     gui.show()
-    sys.exit(app.exec_()):
+    sys.exit(app.exec_())
--- relatorio_teste/converter.py
+++ relatorio_teste/converter.py
@@ -1,36 +1,114 @@
 from PyQt5.QtCore import pyqtSlot, QThread, pyqtSignal
+from datetime import datetime
+import numpy as np
 import requests
+import csv
 
 
 class Converter(QThread):
     conversion_complete = pyqtSignal(bool)
 
-    def __init__(self, comport=None, baudrate=None, serial_number=None, variant=None):
+    def __init__(self, resource='/'):
         QThread.__init__(self)
         self._url = 'http://186.249.222.195'
+        self._resource = resource
 
+    @property
+    def resource(self):
+        return self._resource
+
+    @resource.setter
+    def resource(self, value):
+        self._resource = value
 
     def _get_json(self):
+        try:
+            report_url = self._url + self._resource
+            res = requests.get(report_url)
+            array = np.array(res.json())
+            return array
+        except Exception as e:
+            return "Erro: " + str(e)
 
+    def _get_header(self):
+        res = self._resource
+        if res == '/HradcReport':
+            header = ['Data', 'Serial', 'Variante', 'Medida', 'GND',
+                    'Vref Pos', 'Vref Neg', 'Temperatura',
+                    'Vin Pos', 'Vin Neg', 'Iin Pos', 'Iin Neg',
+                    'Resultado']
+            return header
 
-    def _send_to_server(self, item):
-        client = ElpWebClient()
-        client_data = item.data
-        client_method = item.method
-        client_response = client.do_request(client_method, client_data)
-        print(client_response)
-        server_status = self._parse_response(client_response)
-        return server_status
+        elif res == '/HradcCalibReport':
+            header = ['Data', 'Serial', 'Operador', 'Variante',
+                    'Temperatura HRADC', 'Temperatura DMM',
+                    'Temperatura Fonte', 'Ganho Vin', 'Offset Vin',
+                    'Ganho Iin', 'Offset Iin', 'Vref Pos', 'Vref Neg',
+                    'GND']
+            return header
 
-    def _parse_response(self, response):
-        res_key = 'StatusCode'
-        err_key = 'error'
+        elif res == '/DcctReport':
+            header = ['Data', 'Serial', 'Variante', 'Canal', 'On/Off',
+                    'iLoad 0', 'iLoad 1', 'iLoad 2', 'iLoad 3', 'iLoad 4',
+                    'iLoad 5', 'iLoad 6', 'iLoad 7', 'iLoad 8', 'iLoad 9',
+                    'iLoad 10', 'Resultado']
+            return header
 
-        if res_key in response.keys() and err_key not in response.keys():
-            return True
+        elif res == '/PowerModuleReport':
+            header = ['Data', 'Serial',
+                    'iLoad 0', 'iLoad 1', 'iLoad 2', 'iLoad 3', 'iLoad 4', 'iLoad 5',
+                    'vLoad 0', 'vLoad 1', 'vLoad 2', 'vLoad 3', 'vLoad 4', 'vLoad 5',
+                    'vdclink 0', 'vdclink 1', 'vdclink 2', 'vdclink 3', 'vdclink 4', 'vdclink 5',
+                    'Temperatura 0', 'Temperatura 1', 'Temperatura 2', 'Temperatura 3', 'Temperatura 4', 'Temperatura 5',
+                    'Resultado', 'Detalhes']
+            return header
+
+        elif res == '/PowerSupplyReport':
+            header = ['Data', 'Serial', 'Canal', 'Teste',
+                    'On/Off', '+20 Duty Cycle', '-20 Duty Cycle', 'iout 0', 'iout 1',
+                    'vout 0', 'vout 1', 'vdclink 0', 'vdclink 1', 'Temperatura 0', 'Temperatura 1',
+                    'Resultado', 'Detalhes']
+            return header
+
+        elif res == '/RackReport':
+            header = ['Data', 'Serial',
+                    'iout 0', 'iout 1', 'iout 2', 'iout 3',
+                    'delta iout 0', ' delta iout 1',
+                    'delta iout 2', 'delta iout 3',
+                    'Resultado', 'Detalhes']
+            return header
+
+        elif res == '/UdcReport':
+            header = ['Data', 'Resultado', 'Serial'
+                    'Expansor I/O 1', 'Expansor I/O 2', 'Leds',
+                    'Buzzer', 'EEPROM', 'FLASH', 'RAM', 'RTC Com',
+                    'RTC Int', 'Sensor Temp Com', 'Sensor Temperatura Valor',
+                    'Alimentação Plano Isolado', 'RS485 0', 'RS485 1', 'RS485 2',
+                    'SD Inserido', 'SD Com', 'Ethernet Inicialização',
+                    'Ethernet Ping', 'Loopback', 'Detalhes']
+            return header
+
+        return []
+
+    def _generate_csv(self):
+        res = self._get_json()
+        if not (len(res) > 0):
+            self.conversion_complete.emit(False)
         else:
-            return False
+            now = datetime.now()
+            now_str = str(now.day) + "_" + str(now.month) + "_" + str(now.day) + "_" + \
+                        str(now.hour) + "_" + str(now.minute) + "_" + str(now.second)
+            nome_begin = self.resource[1:]
+            nome = nome_begin + now_str + '.csv'
+            path = './relatorios/' + nome
+            f = open(path, 'wt')
+            try:
+                writer = csv.writer(f)
+                writer.writerow(self._get_header())
+                writer.writerows(res)
+            finally:
+                f.close()
+            self.conversion_complete.emit(True)
 
     def run(self):
-        self._test_sequence()
-        #pass
+        self._generate_csv()
