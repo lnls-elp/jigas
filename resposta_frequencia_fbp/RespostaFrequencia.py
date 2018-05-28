a067b8273a9f94f63508c59417103b36afb36022
Heitor dos Santos Sousa <heitor.sousa@elp10-linux.abtlus.org.br>
2018-05-28 11:06:52 -0300

    changes frequency response test to deal with the new firmware specifications.

--- resposta_frequencia_fbp/RespostaFrequencia.py
+++ resposta_frequencia_fbp/RespostaFrequencia.py
@@ -1,8 +1,9 @@
 from common.pydrs import SerialDRS
 from datetime     import datetime
-
 import time
 import visa
+
+import test_config
 
 drs = SerialDRS()
 now = datetime.now()
@@ -11,97 +12,14 @@ now = datetime.now()
 ################################################################################
 ####################### LENDO ARQUIVO DE CONFIGURAÇÃO ##########################
 ################################################################################
-_cfile = open('freqtest_config.csv', 'r')
-config = _cfile.read()
-_cfile.close()
-
-config = config.split(';')
-
-for i in config:
-    config[config.index(i)] = config[config.index(i)].split('|')
-
-for j in config:
-    if config[config.index(j)][0] == 'COMPort':
-        drs_port = config[config.index(j)][1]
-        drs_port = drs_port.replace('\n', '')
-        drs_port = drs_port.split(',')
-
-        if len(drs_port) == 1:
-            drs_port = drs_port[0]
-
-    elif config[config.index(j)][0] == 'InstAddr':
-        mult_addr = config[config.index(j)][1]
-        mult_addr = mult_addr.replace('\n', '')
-        mult_addr = mult_addr.split(',')
-
-        if len(mult_addr) == 1:
-            mult_addr = mult_addr[0]
-
-    elif config[config.index(j)][0] == 'BastidorList':
-        bastidor_list = config[config.index(j)][1]
-        bastidor_list = bastidor_list.replace('\n', '')
-        bastidor_list = bastidor_list.split(',')
-
-        if type(bastidor_list) is not list:
-            bastidor_list = aux
-            bastidor_list = []
-            bastidor_list.append(aux)
-
-        for bastidor in bastidor_list:
-            bastidor_list[bastidor_list.index(bastidor)] = bastidor.split(':')
-
-    elif config[config.index(j)][0] == 'IndividualModuleList':
-        individual_module_list = config[config.index(j)][1]
-        individual_module_list = individual_module_list.replace('\n', '')
-        individual_module_list = individual_module_list.split(',')
-
-        if type(individual_module_list) is not list:
-            individual_module_list = aux
-            individual_module_list = []
-            individual_module_list.append(aux)
-
-    elif config[config.index(j)][0] == 'ReferenceValueList':
-        idc_list = config[config.index(j)][1]
-        idc_list = idc_list.replace('\n', '')
-        idc_list = idc_list.split(',')
-
-        if type(idc_list) is not list:
-            idc_list = aux
-            idc_list = []
-            idc_list.append(float(aux))
-        else:
-            for k in idc_list:
-                idc_list[idc_list.index(k)] = int(k)
-
-    elif config[config.index(j)][0] == 'CtrlLoop':
-        ctrl_loop = config[config.index(j)][1]
-        ctrl_loop = ctrl_loop.replace('\n', '')
-        ctrl_loop = ctrl_loop.split(',')
-
-        if type(ctrl_loop) is not list:
-            ctrl_loop = aux
-            ctrl_loop = []
-            ctrl_loop.append(aux)
-
-    elif config[config.index(j)][0] == 'ChannelFrequency':
-        channel_freq = config[config.index(j)][1]
-        channel_freq = channel_freq.replace('\n', '')
-        channel_freq = channel_freq.split(',')
-
-        if type(channel_freq) is not list:
-            channel_freq = aux
-            channel_freq = []
-            channel_freq.append(str(aux))
-
-    elif config[config.index(j)][0] == 'ChannelRMS':
-        channel_rms = config[config.index(j)][1]
-        channel_rms = channel_rms.replace('\n', '')
-        channel_rms = channel_rms.split(',')
-
-        if type(channel_rms) is not list:
-            channel_rms = aux
-            channel_rms = []
-            channel_rms.append(str(aux))
+drs_port = test_config.COMPort
+mult_addr = test_config.InstAddr
+bastidor_list = test_config.BastidorList
+individual_module_list = test_config.IndividualModuleList
+idc_list = test_config.IDCList
+ctrl_loop = test_config.CtrlLoop
+channel_freq = test_config.ChannelFrequency
+channel_rms = test_config.ChannelRMS
 ################################################################################
 ################################################################################
 
@@ -125,69 +43,12 @@ ctrl = input('\nOs dados estão corretos?(y/n): ')
 
 
 ################################################################################
-############################# FUNÇÕES INTERNAS #################################
-################################################################################
-def init_module(drs_port, drs_addr, module, ctrl_loop, amplitude):
-    print('\nInicializando módulo...')
-    drs.Connect(drs_port)
-    time.sleep(0.5)
-    drs.SetSlaveAdd(drs_addr)
-    time.sleep(0.5)
-    drs.Config_nHRADC(4)
-    time.sleep(5)
-    drs.OpMode(3)
-    time.sleep(0.5)
-    drs.ConfigSigGen(0, 0, 0, 0)
-    time.sleep(0.5)
-    drs.Write_sigGen_Offset(0)
-    time.sleep(0.5)
-    drs.Write_sigGen_Amplitude(amplitude)
-    time.sleep(0.5)
-    drs.Write_sigGen_Freq(10)
-    time.sleep(0.5)
-
-    if module == 'modulo 1':
-        drs.TurnOn(1)
-    elif module == 'modulo 2':
-        drs.TurnOn(2)
-    elif module == 'modulo 3':
-        drs.TurnOn(4)
-    elif module == 'modulo 4':
-        drs.TurnOn(8)
-
-    time.sleep(1)
-
-    if ctrl_loop == 'closed':
-        if module == 'modulo 1':
-            drs.ClosedLoop(1)
-        elif module == 'modulo 2':
-            drs.ClosedLoop(2)
-        elif module == 'modulo 3':
-            drs.ClosedLoop(4)
-        elif module == 'modulo 4':
-            drs.ClosedLoop(8)
-
-    time.sleep(1)
-    drs.EnableSigGen()
-    time.sleep(1)
-
-def freq_increment(offset, frequency):
-    drs.Write_sigGen_Offset(offset)
-    time.sleep(0.5)
-    drs.Write_sigGen_Freq(frequency)
-    time.sleep(0.5)
-################################################################################
-################################################################################
-
-
-################################################################################
 ############################# INICIO DO TESTE ##################################
 ################################################################################
 if ctrl == 'y':
-    for a in bastidor_list:
-        bastidor = a[0]
-        drs_addr = int(a[1])
+    drs.Connect(drs_port)
 
+    for bastidor in bastidor_list:
         for module in individual_module_list:
             for loop in ctrl_loop:
                 ################################################################################
@@ -198,22 +59,22 @@ if ctrl == 'y':
 
                 del inst.timeout
 
-                inst.write('CONF:FREQ (@'             + channel_freq[0] + ')')
+                inst.write('CONF:FREQ (@'             + str(channel_freq) + ')')
                 time.sleep(0.5)
-                inst.write('SENS:FREQ:RANG:LOW 3, (@' + channel_freq[0] + ')')
+                inst.write('SENS:FREQ:RANG:LOW 3, (@' + str(channel_freq) + ')')
                 time.sleep(0.5)
 
-                inst.write('CONF:VOLT:AC (@'           + channel_rms[0] + ')')
+                inst.write('CONF:VOLT:AC (@'           + str(channel_rms) + ')')
                 time.sleep(0.5)
-                inst.write('SENS:VOLT:AC:BAND 3, (@'   + channel_rms[0] + ')')
+                inst.write('SENS:VOLT:AC:BAND 3, (@'   + str(channel_rms) + ')')
                 time.sleep(0.5)
 
                 if loop == 'open':
-                    inst.write('CALC:SCALE:GAIN 1, (@'    + channel_rms[0] + ')')
+                    inst.write('CALC:SCALE:GAIN 1, (@'    + str(channel_rms) + ')')
                     time.sleep(0.5)
-                    inst.write('CALC:SCALE:STATE ON, (@'  + channel_rms[0] + ')')
+                    inst.write('CALC:SCALE:STATE ON, (@'  + str(channel_rms) + ')')
                     time.sleep(0.5)
-                    inst.write('ROUT:SCAN (@' + channel_rms[0] + ',' + channel_freq[0] + ')')
+                    inst.write('ROUT:SCAN (@' + str(channel_freq) + ',' + str(channel_freq) + ')')
 
                     print('\nInício do teste de resposta em frequência em malha aberta...')
                     print('\nPor favor, selecione:')
@@ -222,11 +83,11 @@ if ctrl == 'y':
                     pause = input('\nTecle enter para continuar')
 
                 elif loop == 'closed':
-                    inst.write('CALC:SCALE:GAIN 0.01, (@' + channel_rms[0] + ')')
+                    inst.write('CALC:SCALE:GAIN 0.01, (@' + str(channel_rms) + ')')
                     time.sleep(0.5)
-                    inst.write('CALC:SCALE:STATE ON, (@'  + channel_rms[0] + ')')
+                    inst.write('CALC:SCALE:STATE ON, (@'  + str(channel_rms) + ')')
                     time.sleep(0.5)
-                    inst.write('ROUT:SCAN (@' + channel_rms[0] + ',' + channel_freq[0] + ')')
+                    inst.write('ROUT:SCAN (@' + str(channel_freq) + ',' + str(channel_rms) + ')')
 
                     print('\nInício do teste de resposta em frequência em malha fechada...')
                     print('\nPor favor, selecione:')
@@ -235,15 +96,38 @@ if ctrl == 'y':
                     pause = input('\nTecle enter para continuar')
                 ################################################################################
                 ################################################################################
+                
+                
                 for idc in idc_list:
-                    _file = open('RespFrequencia_' + module + '_NS' + bastidor + '.csv', 'a')
+                    _file = open('RespFrequencia_' + str(module) + '_NS' + str(bastidor) + '.csv', 'a')
                     _file.write('IDC = ' + str(idc) + 'A\n')
                     _file.write('time stamp;Set Frequency [Hz];Read Frequency [Hz];Irms [A]\n')
 
+                    amplitude = 0
+
                     if loop == 'open':
-                        init_module(drs_port, drs_addr, module, loop, 10)
+                        amplitude = 10
+                        drs.SetSlaveAdd(module)
+                        time.sleep(0.5)
+                        drs.turn_on()
+                        time.sleep(0.5)
+                        drs.select_op_mode('Cycle')
+                        time.sleep(0.5)
+                        drs.cfg_siggen(0, 0, 10, amplitude, 0, 0, 0, 0, 0)
+                        time.sleep(0.5)
+
                     elif loop == 'closed':
-                        init_module(drs_port, drs_addr, module, loop, 0.1)
+                        amplitude = 0.1
+                        drs.SetSlaveAdd(module)
+                        time.sleep(0.5)
+                        drs.turn_on()
+                        time.sleep(0.5)
+                        drs.select_op_mode('Cycle')
+                        time.sleep(0.5)
+                        drs.cfg_siggen(0, 0, 10, amplitude, 0, 0, 0, 0, 0)
+                        time.sleep(0.5)
+                        drs.closed_loop()
+                        time.sleep(0.5)
 
                     for i in range(1, 5):
                         for j in range(1, 10):
@@ -255,7 +139,10 @@ if ctrl == 'y':
                                 print('\nTestando módulo com idc = '  + str(idc)\
                                       + 'A' + ', frequência = ' + str(frequency) + 'Hz...')
 
-                                freq_increment(idc, frequency)
+                                if loop == 'open':
+                                    drs.set_siggen(frequency, amplitude, idc)
+                                elif loop == 'closed':
+                                    drs.set_siggen(frequency, amplitude, idc)
                                 time.sleep(5)
 
                                 inst.write('READ?')
@@ -275,15 +162,7 @@ if ctrl == 'y':
                     print('Fim do teste: ')
                     print(str(datetime.now()))
 
-                    if module == 'modulo 1':
-                        drs.TurnOff(1)
-                    elif module == 'modulo 2':
-                        drs.TurnOff(2)
-                    elif module == 'modulo 3':
-                        drs.TurnOff(4)
-                    elif module == 'modulo 4':
-                        drs.TurnOff(8)
-
+                    drs.turn_off()
                     _file.close()
 
             inst.close()
